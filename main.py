24c36355f64bc0ec16feeacf698492819d12f8a3
Joshua Johnston <2021johnston.joshua@gmail.com>
2022-12-13 16:21:09 -0500

    Ordinal data label encoded

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pandas as pd
-from datetime import datetime, date
 import numpy
+import pandas as pd
+import matplotlib as mpl
+from datetime import datetime, date
+from sklearn.preprocessing import LabelEncoder
 
 raw = pd.read_csv('Data\marketing_campaign.csv', delimiter='\t')
 
@@ -13,13 +15,14 @@ def one_hot_encode(original_df, feature):
     return (res)
 
 
-def condense_education(df):
+def trasform_education(df):
     df.loc[df.Education == 'Basic', 'Education'] = 'High School'
     df.loc[df.Education == '2n Cycle', 'Education'] = 'Master'
     df.loc[df.Education == 'Graduation', 'Education'] = 'Bachelors'
+    df['Education'] = df['Education'].map({'High School': 0, 'Bachelors': 1, 'Master': 2, 'PhD': 3})
 
 
-def condesnse_marital_status(df):
+def trasform_marital_status(df):
     df.loc[df.Marital_Status == 'YOLO', 'Marital_Status'] = 'Single'
     df.loc[df.Marital_Status == 'Absurd', 'Marital_Status'] = 'Single'
     df.loc[df.Marital_Status == 'Alone', 'Marital_Status'] = 'Single'
@@ -30,15 +33,14 @@ def drop_redundants(df):
     df.drop('Dt_Customer', axis=1, inplace=True)
     df.drop('Z_CostContact', axis=1, inplace=True)
     df.drop('Z_Revenue', axis=1, inplace=True)
-    df.drop('Education', axis=1, inplace=True)
     df.drop('Marital_Status', axis=1, inplace=True)
+    df.drop('ID', axis=1, inplace=True)
 
 
 def process_data(df):
     df.Year_Birth = 2022 - df.Year_Birth
-    condense_education(df)
-    condesnse_marital_status(df)
-    df = one_hot_encode(df, 'Education')
+    trasform_education(df)
+    trasform_marital_status(df)
     df = one_hot_encode(df, 'Marital_Status')
     drop_redundants(df)
 
@@ -46,6 +48,4 @@ def process_data(df):
 
 
 df = process_data(raw)
-
-for feature in df.columns:
-    print(feature, len(set(df[feature])))
+print(df)
